0f0bef23261a312e2c338b91f35ad12e4274b264
1-21-01-MD-02\ESFOT <kelly.soto@epn.edu.ec>
2018-05-10 10:36:19 -0500

    Esta es la estrella de cinco lados

---
+++ Pentagono.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 08:24:59 2018
+
+@author: Kelly Soto
+"""
+import turtle
+t = turtle.Pen()
+for x in range(0, 5):
+    t.forward(100)
+    t.left(72)
+t.left(36)
+t.forward(162)
+t.left(144)
+t.forward(162)
+t.left(144)
+t.forward(162)
+t.left(144)
+t.forward(162)
+t.left(144)
+t.forward(162)
+    
+    
+         
+    
+turtle.getscreen()._root.mainloop()
+    
+
+
+
+
+
+    
+ 
